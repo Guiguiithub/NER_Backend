cc8f187fd93f40c20a29b81768357b6e8e2598bb
guilherm.henrique <guilherme.amaral.pro@gmail.com>
2023-11-23 15:20:31 +0100

    Resolve bugs

--- WebAPI/Models/InstallationM.cs
+++ WebAPI/Models/InstallationM.cs
@@ -5,19 +5,19 @@ namespace WebAPI.Models
     public class InstallationM
     {
         public int Id { get; set; }
-        public string Description { get; set; }
-        public string Image { get; set; }
-        public string Street { get; set; }
-        public string No { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public string? Street { get; set; }
+        public string? No { get; set; }
         public int PostCode { get; set; }
-        public string City { get; set; }
+        public string? City { get; set; }
         public double CoordinateX { get; set; }
         public double CoordinateY { get; set; }
         public int AzimutOrientation { get; set; }
         public int TiltOrientation { get; set; }
-        public string EnergyType { get; set; }
-        public string IntegrationType { get; set; }
-        public string CellsType { get; set; }
+        public string? EnergyType { get; set; }
+        public string? IntegrationType { get; set; }
+        public string? CellsType { get; set; }
         public int Length { get; set; }
         public int Width { get; set; }
     }
